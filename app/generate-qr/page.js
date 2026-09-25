c3b777e2cf8026407cd0acdf4a0f172300d0a371
s6904035617050-cmyk <s6904035617050@email.kmutnb.ac.th>
2026-09-25 17:29:55 +0700

    Update page.js

--- app/generate-qr/page.js
+++ app/generate-qr/page.js
@@ -2,7 +2,8 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
+
 
 export default function GenerateQR() {
   const [tableNumber, setTableNumber] = useState('');
